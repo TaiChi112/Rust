498fb9e8b89ca6e3a7c47c263b01cf3137113d23
TaiChi <anothai.0978452316@gmail.com>
2024-10-26 22:35:31 +0700

    delete : all everything in this branch

--- rust-roadmap/src/main.rs
+++ rust-roadmap/src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+
+
+fn main() {
+    let person = rust_roadmap::Person {
+        name: "John".to_string(),
+        age: 30,
+    };
+    println!("Hello, {}! You are {} years old.", person.name, person.age);
+}
