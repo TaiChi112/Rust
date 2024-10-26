d39a7daf172dc485e0c4dcc4c892f860d9b883cd
TaiChi <anothai.0978452316@gmail.com>
2024-10-26 22:49:36 +0700

    test : multistruct in rust

---
+++ basic_rust/src/lib.rs
@@ -0,0 +1,4 @@
+pub struct Person2 {
+    pub id: u8,
+    pub name: String,
+}
--- basic_rust/src/main.rs
+++ basic_rust/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let person1 = Person1 {
+        id: 1,
+        name: "Alice".to_string(),
+    };
+    println!("My id {} & My name's {}!", person1.id, person1.name);
+
+    let person2 = basic_rust::Person2 {
+        id: 2,
+        name: "Bob".to_string(),
+    };
+    println!("My id {} & My name's {}!", person2.id, person2.name);
+}
+
+struct Person1 {
+    id: u8,
+    name: String,
 }
