2045d4bf39c9a3121a5e16e0137ffce13f89296d
TaiChi <anothai.0978452316@gmail.com>
2025-02-18 23:52:59 +0700

    test: success I run pass it

--- cpp_convert_rs/src/main.rs
+++ cpp_convert_rs/src/main.rs
@@ -1,6 +1,10 @@
 use cpp_convert_rs::chapter1::point::Point;
 fn main() {
     let (x, y) = (1, 2);
-    let p1 = Point::new(x, y);
+    let name = "x";
+    let p1 = Point::new(&name,x, y);
     p1.show();
+
+    let p3 = Point::default();
+    p3.show();
 }
