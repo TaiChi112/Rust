12201984be620724a8f50ccc7a2b3f5dba6e8d93
TaiChi <anothai.0978452316@gmail.com>
2025-02-18 23:52:30 +0700

    fix: add field checking Default build-in Rust

--- cpp_convert_rs/src/chapter1/point.rs
+++ cpp_convert_rs/src/chapter1/point.rs
@@ -1,20 +1,34 @@
-#[derive(Default)]
 pub struct Point {
+    name: String,
     x: i8,
     y: i8,
 }
 impl Point {
     pub fn default() -> Self {
-        Point { x: 0, y: 0 }
+        Point {
+            name: "Origin".to_string(),
+            x: 0,
+            y: 0,
+        }
     }
-    pub fn new(x: i8, y: i8) -> Self {
-        Point { x, y }
+    pub fn new(name: &str, x: i8, y: i8) -> Self {
+        Point {
+            name: name.to_string(),
+            x,
+            y,
+        }
+    }
+    pub fn get_name(&self) -> &str {
+        &self.name
     }
     pub fn get_x(&self) -> i8 {
         self.x
     }
     pub fn get_y(&self) -> i8 {
         self.y
+    }
+    pub fn set_name(&mut self, name: &str) {
+        self.name = name.to_string();
     }
     pub fn set_x(&mut self, x: i8) {
         self.x = x;
@@ -23,11 +37,11 @@ impl Point {
         self.y = y;
     }
     pub fn show(&self) {
-        println!("x:{},y:{}", self.x, self.y);
+        println!("Point: {} ({}, {})", self.name, self.x, self.y);
     }
 }
 impl Drop for Point {
     fn drop(&mut self) {
-        println!("Point is dropped at x:{},y:{}", self.x, self.y);
+        println!("Dropping Point: {}", self.name);
     }
 }
