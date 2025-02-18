3dda3fdfcc4777b83f055cec53e975579db8d478
TaiChi <anothai.0978452316@gmail.com>
2025-02-19 00:05:14 +0700

    test: try to test other method in the module

--- cpp_convert_rs/src/main.rs
+++ cpp_convert_rs/src/main.rs
@@ -5,11 +5,19 @@ fn main() {
     let (x, y) = (1, 2);
     let name = "x";
     let p1 = Point::new(&name,x, y);
-    p1.show();
+    // p1.show();
+    println!("get point name: {} ", p1.get_name());
+    println!("get point x: {} ", p1.get_x());
+    println!("get point y: {} ", p1.get_y());
 
     let p3 = Point::default();
-    p3.show();
+    // p3.show();
+    println!("get point name: {} ", p3.get_name());
+    println!("get point x: {} ", p3.get_x());
+    println!("get point y: {} ", p3.get_y());
 
     let r1 = Rectangle::new(112,118);
-    r1.show();
+    // r1.show();
+    println!("get rectangle width: {} ", r1.get_width());
+    println!("get rectangle height: {} ", r1.get_height());
 }
