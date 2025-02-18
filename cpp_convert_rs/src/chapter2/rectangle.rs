cffcd1b7fc6dc41ea8a59803c2c1b052fadfa5df
TaiChi <anothai.0978452316@gmail.com>
2025-02-19 00:05:38 +0700

    fix: additional impl module

--- cpp_convert_rs/src/chapter2/rectangle.rs
+++ cpp_convert_rs/src/chapter2/rectangle.rs
@@ -24,4 +24,22 @@ impl Rectangle{
     pub fn show(&self) {
         println!("Rectangle: {} x {} (Area: {}, Perimeter: {})", self.width, self.height, self.area(), self.perimeter());
     }
+    pub fn set_width(&mut self, width: i8){
+        self.width = width;
+    }
+    pub fn set_height(&mut self, height: i8){
+        self.height = height;
+    }
+    pub fn get_width(&self) -> i8{
+        self.width
+    }
+    pub fn get_height(&self) -> i8{
+        self.height
+    }
+}
+impl Drop for Rectangle {
+    fn drop(&mut self) {
+        println!("Dropping Rectangle: {} x {}", self.width, self.height);
+    }
+    
 }
