592b8880b9120d61cf0196bb8881560028c25534
TaiChi <anothai.0978452316@gmail.com>
2025-02-18 23:32:08 +0700

    inti: try to create object in the Rust

---
+++ cpp_convert_rs/src/chapter1/point.rs
@@ -0,0 +1,24 @@
+pub struct Point {
+    x: i8,
+    y: i8,
+}
+impl Point {
+    pub fn new(x: i8, y: i8) -> Self {
+        Point { x, y }
+    }
+    pub fn get_x(&self) -> i8 {
+        self.x
+    }
+    pub fn get_y(&self) -> i8 {
+        self.y
+    }
+    pub fn set_x(&mut self, x: i8) {
+        self.x = x;
+    }
+    pub fn set_y(&mut self, y: i8) {
+        self.y = y;
+    }
+    pub fn show(&self) {
+        println!("x:{},y:{}", self.x, self.y);
+    }
+}
---
+++ cpp_convert_rs/src/main.rs
@@ -0,0 +1,6 @@
+use cpp_convert_rs::chapter1::point::Point;
+fn main() {
+    let (x, y) = (1, 2);
+    let p1 = Point::new(x, y);
+    p1.show();
+}
